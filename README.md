cd5cb91489b2fdb996202f9476d24f7d70e9d1bf
Alessandra Toniato <ato@zurich.ibm.com>
2021-11-23 09:43:45 +0100

    first version of repo

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # rxn_class_token
-Repository to generate and analyze data for the class token model
+
+Repository to prepare and analyze data for the class token 
+
+## Install / Build
+### Create Environment and Install
+```bash
+conda create -n rxn-class-token python=3.6
+conda install -c rdkit rdkit=2020.03.1 # must be installed manually
+pip install -e .
+```
+For development
+```bash
+pip install -e .[dev]
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# LICENSED INTERNAL CODE. PROPERTY OF IBM.
+# IBM Research Zurich Licensed Internal Code
+# (C) Copyright IBM Corp. 2021
+# ALL RIGHTS RESERVED
+
+# Since the rxn_chemutils dependency requires an environment variable, the
+# install_requires variable must be set here instead of setup.cfg.
+
+import os
+from setuptools import setup
+
+setup(
+    install_requires=[
+        'pandas>=0.23.3', # not installing from setup.cfg
+        'tqdm>=4.30.0', # not installing from setup.cfg
+        'jupyterlab>=3.2.0', # not installing from setup.cfg
+        'rxn_chemutils '
+        '@ git+https://{}@github.ibm.com/rxn/rxn_chemutils@latest'.format(os.environ['GHE_TOKEN']),
+    ]
+)
